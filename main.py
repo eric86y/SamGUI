51d7b0d2c63981d8f877d8a9bc4faad62c1a8956
Eric86y <eric.werner.mail@gmail.com>
2024-11-29 10:21:42 +0100

    added window title and icon

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 """
-build with:
-1) python -m nuitka --follow-imports --windows-console-mode=disable main.py
-2) pyinstaller --noconsole --onefile main.spec
+build with Nuitka:
+
+windows: nuitka --standalone --windows-console-mode=disable --plugin-enable=pyside6 --windows-icon-from-ico=logo.ico --include-data-dir=./SamGui/Assets=SamGui/Assets --include-data-dir=./Models=Models --include-data-files=./logo.png=logo.png main.py
+
 """
 
 import sys
@@ -9,12 +10,15 @@ from SamGui.MVVM.view import AppView
 from SamGui.MVVM.model import DataModel
 from SamGui.MVVM.viewmodel import SamViewModel
 from PySide6.QtCore import QPoint
+from PySide6 import QtGui
+
 from PySide6.QtWidgets import QApplication
 from SamGui.Utils import get_screen_center
 
 
 if __name__ == '__main__':
     app = QApplication()
+    app.setWindowIcon(QtGui.QIcon('logo.png'))
     model = DataModel()
     view_model = SamViewModel(model)
     app_view = AppView(view_model)
